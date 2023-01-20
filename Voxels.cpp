bee03f9da3048c5eaf9442a5bc531a1c9133f5bc
Liuvier <liushuze_lsz@hotmail.com>
2023-01-20 19:57:13 +0100

    voxel: complete undebugged version

--- Voxels.cpp
+++ Voxels.cpp
@@ -1,42 +1,72 @@
 #include "Voxels.h"
 
-Vector4f VoxelInterface::World2Camera(Vector4f world_location) {
+Vector4f VoxelInterface::World2Camera(Vector4f world_location)
+{
     return _Pose * world_location;
 }
 
-Vector4f VoxelInterface::Camera2World(Vector4f camera_location) {
+Vector4f VoxelInterface::Camera2World(Vector4f camera_location)
+{
     return _Pose.inverse() * camera_location;
 }
 
-unsigned VoxelArray::location2idx(Vector4f location) {
-    float x = std::floor(location(0) / location(3) / _grid_len);
-    float y = std::floor(location(1) / location(3) / _grid_len);
-    float z = std::floor(location(2) / location(3) / _grid_len);
-    return x * _size[0] + y * _size[1] + z;
+float VoxelInterface::getGridlen()
+{
+    return _grid_len;
 }
-void VoxelArray::SetWeightVal(Vector4f location, float weight) {
+
+bool VoxelArray::isValidLocation(Vector4f location)
+{
+    if ((location(0) / location(3)) < _valid_location_range(0, 0) || (location(0) / location(3)) > _valid_location_range(0, 1))
+        return false;
+    if ((location(1) / location(3)) < _valid_location_range(1, 0) || (location(1) / location(3)) > _valid_location_range(1, 1))
+        return false;
+    if ((location(2) / location(3)) < _valid_location_range(2, 0) || (location(2) / location(3)) > _valid_location_range(2, 1))
+        return false;
+    return true;
+}
+
+unsigned VoxelArray::location2idx(Vector4f location)
+{
+    if (!isValidLocation(location))
+        throw std::out_of_range("Invalid world location");
+    unsigned x = std::floor((location(0) / location(3) - _origin(0)) / _grid_len);
+    unsigned y = std::floor((location(1) / location(3) - _origin(1)) / _grid_len);
+    unsigned z = std::floor((location(2) / location(3) - _origin(2)) / _grid_len);
+    return x + y * _size[0] + z * _size[0] * _size[1];
+}
+
+void VoxelArray::SetWeightVal(Vector4f location, float weight)
+{
     unsigned idx = location2idx(location);
     voxel[idx].weight = weight;
 }
-void VoxelArray::SetSDFVal(Vector4f location, float sdf) {
+
+void VoxelArray::SetSDFVal(Vector4f location, float sdf)
+{
     unsigned idx = location2idx(location);
     voxel[idx].sdf = sdf;
 }
-void VoxelArray::SetColorVal(Vector4f location, Vector4uc color) {
+
+void VoxelArray::SetColorVal(Vector4f location, Vector4uc color)
+{
     unsigned idx = location2idx(location);
     voxel[idx].color = color;
 }
 
-float VoxelArray::GetWeightVal(Vector4f location) {
+float VoxelArray::GetWeightVal(Vector4f location)
+{
     unsigned idx = location2idx(location);
     return voxel[idx].weight;
 }
 
-void VoxelArray::GetSDFVal(Vector4f location) {
+float VoxelArray::GetSDFVal(Vector4f location)
+{
     unsigned idx = location2idx(location);
     return voxel[idx].sdf;
 }
-void VoxelArray::GetColorVal(Vector4f location) {
+Vector4uc VoxelArray::GetColorVal(Vector4f location)
+{
     unsigned idx = location2idx(location);
     return voxel[idx].color;
 }
--- Voxels.h
+++ Voxels.h
@@ -4,51 +4,65 @@
 #include "vector"
 /*
 TODO:
-    1. worldcoord与voxel对应只实现了线性缩放，只能处理一段为0的空间
-        （eg: 2-100 -> 2/factor - 100/factor）, index没有被好好利用, 负坐标甚至会出现非法idx
-        在interface加入“原点”, 以实现仿射变换,解决上述问题
     2. world与camera坐标转换如果常用可以独立成camera类
 */
-struct VoxelElement {
+struct VoxelElement
+{
     float sdf = MINF;
     float weight = 0;
     Vector4uc color = Vector4uc(0, 0, 0, 0);
 };
-// consistent with "Frame.h", 
-// invalid SDF value: MINF, 
+// consistent with "Frame.h",
+// invalid SDF value: MINF,
 // invalid weight value: 0,
 // invalid Color value: Vector4uc(0, 0, 0, 0)
 
-class VoxelInterface {
-    public:
-        VoxelInterface(float grid_len, Matrix4f Pose): _grid_len{grid_len}, _Pose{Pose} {};
-        virtual void SetWeightVal(Vector4f location, float weight) = 0;
-        virtual void SetSDFVal(Vector4f location, float sdf) = 0;
-        virtual void SetColorVal(Vector4f location, Vector4uc color) = 0;
-        // change voxel value / insert new voxel according to world location
-        virtual float GetWeightVal(Vector4f location) = 0;
-        virtual float GetSDFVal(Vector4f location) = 0;
-        virtual Vector4uc GetColorVal(Vector4f location) = 0;
-    protected: 
-        Vector4f World2Camera(Vector4f world_location);
-        Vector4f Camera2World(Vector4f camera_location);
-        const float _grid_len; // do we need to support rectangle voxel?
-        Matrix4f _Pose; // camera pose
+class VoxelInterface
+{
+public:
+    VoxelInterface(float grid_len, Matrix4f Pose) : _grid_len{grid_len}, _Pose{Pose} {};
+    virtual void SetWeightVal(Vector4f location, float weight) = 0;
+    virtual void SetSDFVal(Vector4f location, float sdf) = 0;
+    virtual void SetColorVal(Vector4f location, Vector4uc color) = 0;
+    // change voxel value / insert new voxel according to world location
+    virtual float GetWeightVal(Vector4f location) = 0;
+    virtual float GetSDFVal(Vector4f location) = 0;
+    virtual Vector4uc GetColorVal(Vector4f location) = 0;
+    virtual bool isValidLocation(Vector4f world_location) = 0;
+    Vector4f World2Camera(Vector4f world_location);
+    Vector4f Camera2World(Vector4f camera_location);
+    float getGridlen();
+
+protected:
+    const float _grid_len;
+    Matrix4f _Pose;        // camera pose
 };
 
-class VoxelArray: public VoxelInterface {
-    public:
-        VoxelArray() = delete;
-        VoxelArray::VoxelArray(std::array<unsigned, 3> size, float grid_len, Matrix4f Pose):
-            VoxelInterface{grid_len, Pose}, _size{size}, voxel{size[0] * size[1] * size[2]} {};
-        virtual void SetWeightVal(Vector4f location, float weight) override;
-        virtual void SetSDFVal(Vector4f location, float sdf) override;
-        virtual void SetColorVal(Vector4f location, Vector4uc color) override;
-        virtual float GetWeightVal(Vector4f location) override;
-        virtual float GetSDFVal(Vector4f location) override;
-        virtual Vector4uc GetColorVal(Vector4f location) override;
-    private:
-        unsigned location2idx(Vector4f location);
-        std::array<unsigned, 3> _size;
-        std::vector<VoxelElement> voxel;
+class VoxelArray : public VoxelInterface
+{
+public:
+    VoxelArray() = delete;
+    VoxelArray(std::array<unsigned, 3> size, float grid_len, Vector3f origin, Matrix4f Pose) : 
+        VoxelInterface{grid_len, Pose}, _origin{origin}, _size{size}, voxel{size[0] * size[1] * size[2]},
+        _valid_location_range {_origin(0), _origin(0) + _grid_len * size[0], 
+        _origin(1), _origin(1) + _grid_len * size[1], _origin(2), _origin(2) + _grid_len * size[2]} {};
+
+    virtual bool isValidLocation(Vector4f location) override;
+
+    /* setting value in voxel */
+    virtual void SetWeightVal(Vector4f location, float weight) override;
+    virtual void SetSDFVal(Vector4f location, float sdf) override;
+    virtual void SetColorVal(Vector4f location, Vector4uc color) override;
+
+    /* getting value in voxel*/
+    virtual float GetWeightVal(Vector4f location) override;
+    virtual float GetSDFVal(Vector4f location) override;
+    virtual Vector4uc GetColorVal(Vector4f location) override;
+
+private:
+    Vector3f _origin; // corner of grid , {-3, -3, 0} may be appropriate
+    unsigned location2idx(Vector4f location);
+    std::array<unsigned, 3> _size;
+    std::vector<VoxelElement> voxel;
+    Matrix<float, 3, 2> _valid_location_range; // valid range of world location
 };
