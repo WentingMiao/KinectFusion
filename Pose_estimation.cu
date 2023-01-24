115edba5dd388da883ed0ac59b44fd08410496fb
zenghjian <zenghuajian97@gmail.com>
2023-01-24 18:20:46 +0100

    add metrics; add comments

---
+++ Pose_estimation.cu
@@ -0,0 +1,2 @@
+#define BLOCK_SIZE_X 32
+#define BLOCK_SIZE_Y 32
--- Pose_estimation.h
+++ Pose_estimation.h
@@ -19,24 +19,25 @@ public:
 
 Pose() = default;
 
-
 /**
- * @description: main entrace of pose estimation stage
- * @param {Matrix4f&} pose: input -> pose of last frame, output -> pose of current frame
- * @param {Vertex&} frame_data: current frame data. call Frame::getVertex() to obtain
- * @param {Vertex&} model_data: Vertex Map + Normal Map + Color Map from predict part
- * @param {float} distance_threshold: outlier about depth for data association 
- * @param {float} angle_threshold: outlier about angle for data association 
- * @param {int&} iteration: number of ICP iteration
- * @return {*} true if ICP is successful
- * 
- * @note need change after pyramid added
+ * @description: main entrance of pose estimation process
+ * @param frame_data: vertex, normal and color map of current frame k 
+ * @param model_data: vertex, normal and color map of previous frame k-1
+ * @param distance_threshold: outlier check
+ * @param angle_threshold: outlier check //with angle °
+ * @param num_iteration
+ * @param width: image size
+ * @param height: image size
+ * @param pyramid_level
+ * @param cur_pose: pose which we need updated
+ * @return true if pose_estimation successed
  */
+
 bool pose_estimation(const std::vector<vector<Vertex>>& frame_data,
                      const std::vector<vector<Vertex>>& model_data,
                      const std::vector<Matrix3f>& intrinstics,
                      const float distance_threshold,
-                     const float angle_threshold, //with angle °
+                     const float angle_threshold, 
                      const std::vector<int>& num_iteration,
                      const std::vector<int>& width,
                      const std::vector<int>& height,
@@ -44,60 +45,66 @@ bool pose_estimation(const std::vector<vector<Vertex>>& frame_data,
                      Eigen::Matrix4f& cur_pose
 );
 
+/**
+ * @description: data association step. Matched pairs are stored in matches
+ * @return {*}
+ */
 void data_association(const std::vector<Vertex>& frame_data,
-                      const Matrix3f &Intrinsics,
+                      const Matrix3f& Intrinsics,
                       const unsigned int& width,
                       const unsigned int& height,
                       std::unordered_map<int, int>& matches);
 
+/**
+ * @description: outlier check step. Filter out pairs that meet the requirements
+ * @return {*}
+ */
 void outlier_check( const std::vector<Vertex>& frame_data,
                     const std::vector<Vertex>& model_data,
                     std::unordered_map<int, int>& matches,
                     std::unordered_map<int, int>& selected_matches,
                     const float& distance_threshold,
-                    const float& angle_threshold);
+                    const float& angle_threshold,
+                    double& error);
 
+/**
+ * @description: Point to Plane ICP step. we update member m_current_pose with incremental
+ * @return {*}
+ */
 void incremental_caculation(const std::vector<Vertex>& frame_data,
                             const std::vector<Vertex>& model_data,
-                            std::unordered_map<int, int>& selected_matches,
-                            int& iteration_index);
+                            std::unordered_map<int, int>& selected_matches);
 
 
+/**
+ * @description: Util. Vector 4f to 3f
+ * @return {*}
+ */
 Vector3f Vector4fToVector3f(Vector4f vertex);
 
+/**
+ * @description: Util. Vector 3f to 4f
+ * @return {*}
+ */
 Vector4f Vector3fToVector4f(Vector3f vertex);
 
+/**
+ * @description: Util. apply transformation to a vertex
+ * @return {*}
+ */
 Vector3f TransformToVertex(Vector3f vertex, Eigen::Matrix4f Transformation);
 
+/**
+ * @description: Util. apply transformation to a normal
+ * @return {*}
+ */
 Vector3f TransformToNormal(Vector3f normal, Eigen::Matrix4f Transformation);
 
 private:
 
-
-/* 
-pose of current frame.
-*/
-
 Eigen::Matrix4f m_current_pose;
 
 Eigen::Matrix4f m_previous_pose;
-
-// std::vector<Vertex> m_frame_data;
-
-// std::vector<Vertex> m_model_data;
-
-// unsigned int m_width;
-
-// unsigned int m_height;
-
-// unsigned int m_pyramid_level;
-
-// Matrix3f m_intrinsics;
-
-// float m_distance_threshold;
-
-// float m_angle_threshold;
-
 
 };
 
