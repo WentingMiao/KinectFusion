5327a8d56af7bf0f92d0f97bd95572460c4cb8f1
MiaoWenting <mwt609042270@gmail.com>
2023-01-25 21:01:38 +0100

    main debug

--- main.cpp
+++ main.cpp
@@ -26,42 +26,59 @@ int execute(){
     
     while(sensor.ProcessNextFrame()){
     
-    Matrix4f depthExtrinsics = sensor.GetDepthExtrinsics();
-    Matrix3f depthIntrinsics = sensor.GetDepthIntrinsics();
-    Matrix4f trajectory = sensor.GetTrajectory();
+        Matrix4f depthExtrinsics = sensor.GetDepthExtrinsics();
+        Matrix3f depthIntrinsics = sensor.GetDepthIntrinsics();
+        Matrix4f trajectory = sensor.GetTrajectory();
 
-    BYTE* colorMap = &sensor.GetColorRGBX()[0];
-    float* depthMap = &sensor.GetDepth()[0];
+        BYTE* colorMap = &sensor.GetColorRGBX()[0];
+        float* depthMap = &sensor.GetDepth()[0];
 
-    unsigned int width  = sensor.GetDepthImageWidth();
-    unsigned int height = sensor.GetDepthImageHeight();
-    float edgeThreshold = 10;
-    bool filtered = true;
-
-
+        unsigned int width  = sensor.GetDepthImageWidth();
+        unsigned int height = sensor.GetDepthImageHeight();
+        float edgeThreshold = 10;
+        bool filtered = false;
 
 
-    Frame currentFrame(depthMap, colorMap, depthIntrinsics, depthExtrinsics, trajectory, width, height, edgeThreshold, filtered);
+        unsigned int levelSize = 3;
+        Frame currentFrame(depthMap, colorMap, depthIntrinsics, depthExtrinsics, trajectory, width, height, edgeThreshold, filtered, levelSize);
 
-    vector<float> depthVectorMap = currentFrame.getDepthMap();
-    unsigned int levelSize = 5;
-    vector<vector<float>> depthPyramid ;
-
+        // vector<float> depthVectorMap = currentFrame.getDepthMap();
     
-    currentFrame.buildDepthPyramid(depthVectorMap, depthPyramid, levelSize);
+        // vector<vector<float>> depthPyramid ;
+
+        
+        // currentFrame.buildDepthPyramid(depthVectorMap, depthPyramid, levelSize);
 
 
-    vector<Vertex> vertices = currentFrame.getVertices();
-    
-    
-    /*write to the mesh*/
-    stringstream ss;
-	ss << filenameBaseOut << sensor.GetCurrentFrameCnt() << ".off";
-    cout<<ss.str()<<endl;
-    if (!currentFrame.writeMesh(vertices,ss.str())){
-			cout << "Failed to write mesh!\nCheck file path!" << endl;
-			return -1;
-	}
+        // vector<Vertex> vertices = currentFrame.getVertices();
+        
+        vector<vector<Vertex>> pyramidDepthMap = currentFrame.getPyramidVertex();
+        
+            
+
+        /*write to the mesh*/
+        // stringstream ss;
+        // ss << filenameBaseOut << sensor.GetCurrentFrameCnt() << "_LL1"<<".off";
+        // cout<<ss.str()<<endl;
+        // if (!currentFrame.writeMesh(pyramidDepthMap[1],ss.str(),1)){
+        // 		cout << "Failed to write mesh!\nCheck file path!" << endl;
+        // 		return -1;
+        // }
+
+
+        for(unsigned int level = 0; level < levelSize; level++){
+            stringstream ss;
+            ss << filenameBaseOut << sensor.GetCurrentFrameCnt() <<"Level"<< level<< "_test"<<".off";
+            cout<<ss.str()<<endl;
+            if (!currentFrame.writeMesh(pyramidDepthMap[level],ss.str(),level)){
+                    cout << "Failed to write mesh!\nCheck file path!" << endl;
+                    return -1;
+            }
+        }
+        
+
+
+
     }
     
 
